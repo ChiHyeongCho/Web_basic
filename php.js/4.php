540df446b1961d5f6dae8b489cd58b9bbec1c522
whcl303 <whcl303@daum.net>
2021-09-11 19:22:37 +0900

    web

---
+++ php.js/4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>php</h1>
+    <?php
+        $name = "cho";
+        echo "안녕하세요".$name;
+    ?>
+
+    <h1>javaScript</h1>
+
+    <script>
+        name = "cho"
+        document.write("안녕하세요" + name);
+    </script>
+</body>
+
+</html>
